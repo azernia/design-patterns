888ac26d30f70f0f4c9425e8e6d2dfb883c7de33
rui <azernia@foxmail.com>
2022-06-07 14:28:29 +0800

    建造者模式

---
+++ create-patterns/builder-pattern/src/main/java/com/rui/Builder.java
@@ -0,0 +1,34 @@
+package com.rui;
+
+import com.rui.ceiling.LevelOneCeiling;
+import com.rui.ceiling.LevelTwoCeiling;
+import com.rui.coat.DuluxCoat;
+import com.rui.coat.LiBangCoat;
+import com.rui.floor.ShengXiangFloor;
+import com.rui.tile.DongPengTile;
+import com.rui.tile.MarcoPoloTile;
+
+public class Builder {
+
+    public IMenu levelOne(Double area) {
+        return new DecorationPackageMenu(area, "豪华欧式")
+                .appendCeiling(new LevelTwoCeiling())    // 吊顶，二级顶
+                .appendCoat(new DuluxCoat())             // 涂料，多乐士
+                .appendFloor(new ShengXiangFloor());     // 地板，圣象
+    }
+
+    public IMenu levelTwo(Double area){
+        return new DecorationPackageMenu(area, "轻奢田园")
+                .appendCeiling(new LevelTwoCeiling())   // 吊顶，二级顶
+                .appendCoat(new LiBangCoat())           // 涂料，立邦
+                .appendTile(new MarcoPoloTile());       // 地砖，马可波罗
+    }
+
+    public IMenu levelThree(Double area){
+        return new DecorationPackageMenu(area, "现代简约")
+                .appendCeiling(new LevelOneCeiling())   // 吊顶，一级顶
+                .appendCoat(new LiBangCoat())           // 涂料，立邦
+                .appendTile(new DongPengTile());        // 地砖，东鹏
+    }
+
+}
